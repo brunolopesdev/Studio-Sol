e494fa1d93e939f142af5f662695992e7b940c4c
Bruno <brunoslopes1994@gmail.com>
2021-10-25 14:25:31 -0300

    game finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import "./App.css";
 import "./style.scss";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { IoMdRefresh } from "react-icons/io";
+import axios from "axios";
+
+let ACERTO = "Você acertou!!!!";
+let MENOR = "É menor";
+let MAIOR = "É maior";
+let ERRO = "ERRO";
 
 function App() {
   const [started, setStarted] = useState(false);
   const [status, setStatus] = useState(false);
-  const [answer, setAnswer] = useState("");
-  const [rightAnswer, setRightAnswer] = useState();
+  const [answer, setAnswer] = useState();
   const [error, setError] = useState(false);
+  const [rightAnswer, setRightAnswer] = useState();
   console.log(rightAnswer);
 
   useEffect(() => {
@@ -22,34 +26,48 @@ function App() {
         "https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300"
       );
       setRightAnswer(res.data.value);
+      setError(false);
       setStarted(true);
+      setAnswer();
+      setStatus();
 
-      if (status === "Você acertou!!!!") {
-        setAnswer("");
+      if (status === ACERTO) {
+        setAnswer();
         setStatus(false);
       }
     } catch (error) {
-      if (error) {
+      if (error.response) {
+        setStatus(ERRO);
         setError(true);
+        setAnswer(error.response.data.StatusCode);
       }
     }
   }
 
-  const submit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const formValid = +answer >= 0;
-    if (!formValid) {
+    const validForm = answer >= 0;
+    if (!validForm) {
       return;
     }
-    if (+answer === +rightAnswer) {
-      setStatus("Você acertou!!!!");
+    if (+answer === rightAnswer) {
+      setStatus(ACERTO);
       setStarted(false);
-    } else if (+answer < +rightAnswer) {
-      setStatus("É maior");
+    } else if (answer < rightAnswer) {
+      setStatus(MAIOR);
     } else {
-      setStatus("É menor");
+      setStatus(MENOR);
     }
   };
+
+  const newGameButton = (
+    <button type="button" className="newGame" onClick={() => start()}>
+      <i>
+        <IoMdRefresh />
+      </i>
+      Nova partida
+    </button>
+  );
 
   return (
     <div className="homeContainer">
@@ -57,33 +75,29 @@ function App() {
         <h1 className="title">Qual é o número?</h1>
         <hr />
         <div className="numberContainer">
-          {error ? (
-            <p className="statusError">Erro</p>
-          ) : (
-            <p className="status">{status}</p>
-          )}
-
-          {status === "Você acertou!!!!" ? (
+          <p className={error ? "statusError" : "status"}>{status}</p>
+          {status === ACERTO ? (
             <span className="rightAnswer">{answer}</span>
           ) : (
-            <span className="">{answer}</span>
+            <span className={status === ERRO ? "errorMsg" : ""}>{answer}</span>
           )}
 
-          {!started && (
-            <button type="button" className="newGame" onClick={() => start()}>
-              <i>
-                <IoMdRefresh />
-              </i>
-              Nova partida
-            </button>
-          )}
+          {error || !started ? newGameButton : null}
         </div>
-        <form onSubmit={submit} className="inputContainer">
+        <form onSubmit={handleSubmit} className="inputContainer">
           <input
             onChange={(e) => setAnswer(e.target.value)}
             placeholder="Digite o palpite"
           />
-          <button type="submit">Enviar</button>
+          <button
+            type="submit"
+            disabled={error}
+            className={
+              error || status === ACERTO ? "disabledButton" : "enabledButton"
+            }
+          >
+            Enviar
+          </button>
         </form>
       </div>
     </div>
